e714393d395c4b5992c41e51d80d95d23479d476
Sami Moqbel <Sami.Moqbel@exalt.ps>
2024-08-20 12:38:24 +0300

    Test specs Done

--- digit-le/src/views/Home/Home.tsx
+++ digit-le/src/views/Home/Home.tsx
@@ -16,6 +16,10 @@ export const Home = () => {
 
   const handleLevelButton = (level: Levels) => {
     if (name.trim()) {
+      if (!name.match(/^[a-zA-Z]+$/)) {
+        toast.error("Please enter a valid name");
+        return;
+      }
       navigate(`/board/${level}/${name}`);
     } else {
       toast.error("Please enter your name");
